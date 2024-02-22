8bc83be0624258754066f1e03fc5e2c55dcf997d
Leo Mark Castro <66856556+leomarkcastro@users.noreply.github.com>
2024-02-22 06:27:17 +0000

    feature: doc-type-creation

---
+++ healthcare_phystherapynote/xps___health___physical_theraphy_progress_notes/doctype/physical_theraphy_progress_notes/test_physical_theraphy_progress_notes.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Leo Mark Castro and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPhysicalTheraphyProgressNotes(FrappeTestCase):
+	pass
